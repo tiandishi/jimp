1fb2c4975586e64c6326c5c397e1a86aafb9a945
tiandishi <1020378873@qq.com>
2014-12-04 15:39:26 +0800

    点击生成灰度图

--- js/jimp.js
+++ js/jimp.js
@@ -1,74 +1,54 @@
-(function () {
-    function imageToGray(iCanvas, url) {
-        this.canvas = iCanvas;
-        this.iCtx = this.canvas.getContext("2d");
-        this.url = url;
-    }
+'use strict';
 
-    imageToGray.prototype = {
-        imread: function (_image) {
-            var width = _image.width,
-                height = _image.height;
-            this.iResize(width, height);
-            this.iCtx.drawImage(_image, 0, 0);
-            var imageData = this.iCtx.getImageData(0, 0, width, height),
-                tempMat = new Mat(height, width, imageData.data);
-            imageData = null;
-            this.iCtx.clearRect(0, 0, width, height);
-            return tempMat;
-        },
-        iResize: function (_width, _height) {
-            this.canvas.width = _width;
-            this.canvas.height = _height;
-        },
-        RGBA2ImageData: function (_imgMat) {
-            var width = _imgMat.col,
-                height = _imgMat.row,
-                imageData = this.iCtx.createImageData(width, height);
-            imageData.data.set(_imgMat.data);
-            return imageData;
-        },
-        render: function () {
-            var img = new Image();
-            var _this = this;
-            img.onload = function () {
-                var myMat = _this.imread(img);
-                var newImage = cvtColor(myMat);
-                var newIamgeData = _this.RGBA2ImageData(newImage);
-                _this.iCtx.putImageData(newIamgeData, 0, 0);
-            };
-            img.src = this.url;
+var jimp = angular.module('jimp', ['ui.bootstrap', 'ngRoute']);
+
+jimp.config(['$routeProvider', '$locationProvider', '$sceProvider', function ($routeProvider, $locationProvider, $sceProvider) {
+        $routeProvider
+                .when('/', {controller: 'DemoCtrl'})
+                .when('/abc', {controller: 'DemoCtrl'})
+                .when('/123', {controller: 'DemoCtrl'})
+                .otherwise({redirectTo: '/'});
+        $locationProvider.html5Mode(true);
+    }]);
+
+jimp.controller('file_manage_ctl', function ($scope, $location) {
+    $scope.isActive = function (route) {
+        if ($location.path().indexOf('/dropdown') == 0) {
+            return  route === '/dropdown';
+        }
+        return route === $location.path();
+    };
+
+    $scope.file_open = function () {
+        alert("asdfasd");
+        fileDialog.CancelError = true;
+        try {
+            fileDialog.Filter = "HTM   Files   (*.htm)|*.htm|Text   Files   (*.txt)|*.txt";
+            fileDialog.ShowOpen();
+            var fso = new ActiveXObject("Scripting.FileSystemObject");
+            var reading = 1;
+            var f = fso.OpenTextFile(fileDialog.filename, reading);
+            //window.confirm(f);   
+            var r = f.ReadAll();
+            f.close();
+            TxtBody.value = r;
+        } catch (e) {
         }
     };
 
-    function Mat(_row, _col, _data, _buffer){
-        this.row = _row || 0;
-        this.col = _col || 0;
-        this.channel = 4;
-        this.buffer = _buffer || new ArrayBuffer(_row * _col * 4);
-        this.data = new Uint8ClampedArray(this.buffer);
-        _data && this.data.set(_data);
-        this.bytes = 1;
-        this.type = "CV_RGBA";
-    }
+    
+});
 
-    function cvtColor(_src) {
-        if (_src.type && _src.type === "CV_RGBA") {
-            var row = _src.row,
-                col = _src.col;
-            var dst = new Mat(row, col);
-            data = dst.data,
-            data2 = _src.data;
-            var pix1, pix2, pix = _src.row * _src.col * 4;
-            while (pix) {
-                data[pix -= 4] = data[pix1 = pix + 1] = data[pix2 = pix + 2] = (data2[pix] * 299 + data2[pix1] * 587 + data2[pix2] * 114) / 1000;
-                data[pix + 3] = data2[pix + 3];
-            }
-        } else {
-            return src;
-        }
-        return dst;
-    }
+jimp.controller('gray_img_ctl', function ($scope, $location) {
+        $scope.img_to_gray = function () {
+       // alert("fsfd");
+        var iCanvas = document.getElementById("grayImage");
+        var imgToGray = new imageToGray(iCanvas, "images/1.jpg");
+        imgToGray.render();
+    };
+    
+});
 
-    window.imageToGray = imageToGray;
-})();
+jimp.controller('img_to_gray', function () {
+
+});
